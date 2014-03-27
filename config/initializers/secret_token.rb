20482d0b5e3ffad4abf5caf77dd422f6ca20b814
andrap11 <andrap11@gmail.com>
2014-03-27 18:16:24 +0200

    Initial commit on survey

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Survey::Application.config.secret_key_base = 'd5a39920be7646a7c5211cc2b264f98f0f43256fa65aacbbde8da50c5ddbeb7d877ee578e7ff445073648d06da885784323dbf889069afc49a62c9068f3a36d9'
